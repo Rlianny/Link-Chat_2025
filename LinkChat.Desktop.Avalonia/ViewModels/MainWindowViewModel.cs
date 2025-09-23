3035afec771a691f131b4aa461d496da73cbf5c1
Rlianny <revelianny10@gmail.com>
2025-09-23 15:40:13 -0400

    LinkChat.Desktop.Avalonia added with some visual elements

---
+++ LinkChat.Desktop.Avalonia/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace LinkChat.Desktop.Avalonia.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ LinkChat.Desktop.Avalonia/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace LinkChat.Desktop.Avalonia.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
