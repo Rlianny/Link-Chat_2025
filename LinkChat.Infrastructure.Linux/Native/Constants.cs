4270dd38af0629664e77a6b108a449dab1ac1de2
Rlianny <revelianny10@gmail.com>
2025-10-01 19:23:50 -0400

    Native implementation updated

--- LinkChat.Infrastructure.Linux/Native/Constants.cs
+++ LinkChat.Infrastructure.Linux/Native/Constants.cs
@@ -2,6 +2,12 @@ namespace LinkChat
 {
     internal static class NativeConstants
     {
-        
+        //const for socket()
+        const int AF_PACKET = 17; // Specifies the "family" of the network. The value 17 specifies that you want to work directly with link layer (Layer 2) frames, without involving IP.
+        const int SOCK_RAW = 3; // Defines the socket type. A value of 3 indicates that you want to handle the entire packet, including the frame header.
+        const ushort LINK_CHAT_ETHER_TYPE = 0x88B5; //customized ether type
+
+        //const for ioctl()
+        const int SIOCGIFINDEX = 0x8933; //It is the operation code that stands for "Get Interface Index"
     }
 }
--- LinkChat.Infrastructure.Linux/Native/Structs.cs
+++ LinkChat.Infrastructure.Linux/Native/Structs.cs
@@ -1,7 +1,32 @@
-namespace LinkChat
+namespace LinkChat.Infrastructure.Linux.Native.Structs
 {
+    using System.Runtime.InteropServices;
+
     internal static class NativeStructs
     {
-        
+        //Struct ifreq (for ioctl)
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+        public struct ifreq
+        {
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 16)]
+            public string ifr_name; // Interface name. IFNAMSIZ is 16 on Linux.
+            public int ifr_ifindex; // This is where the kernel will return the index
+        }
+
+        // Struct sockaddr_ll (for bind, sendto, recvfrom)
+        [StructLayout(LayoutKind.Sequential)]
+        public struct sockaddr_ll
+        {
+            public ushort sll_family;   //Always AF_PACKET
+            public ushort sll_protocol; // Frame protocol (your EtherType)
+            public int sll_ifindex;  // Interface index
+            public ushort sll_hatype;   // Hardware type
+            public byte sll_pkttype;  // Package type
+            public byte sll_halen;    // address length (6 for MAC)
+
+            // MAC address (6 bytes) + padding (2 bytes)
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 8)]
+            public byte[] sll_addr;
+        }
     }
 }
