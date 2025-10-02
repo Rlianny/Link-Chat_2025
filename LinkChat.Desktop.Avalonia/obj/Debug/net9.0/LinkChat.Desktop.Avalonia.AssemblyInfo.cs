b934fbd281098bbda5ca76662892ad426d069595
Rlianny <revelianny10@gmail.com>
2025-10-02 01:55:49 -0400

    User merge conflicts fixed,compilations files updated

--- LinkChat.Desktop.Avalonia/obj/Debug/net9.0/LinkChat.Desktop.Avalonia.AssemblyInfo.cs
+++ LinkChat.Desktop.Avalonia/obj/Debug/net9.0/LinkChat.Desktop.Avalonia.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LinkChat.Desktop.Avalonia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76353e12367a23dc3179dba2ea149df8d3cfd0b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8986fd438f5d6691128d630b9143784f30c6c0ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("LinkChat.Desktop.Avalonia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LinkChat.Desktop.Avalonia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
