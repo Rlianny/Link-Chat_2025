61e4915254173250a21d28169de25c9ad38b27bc
Rlianny <revelianny10@gmail.com>
2025-10-02 01:42:01 -0400

    Protocol Service Updated

--- LinkChat.Core/Implementations/ProtocolService.cs
+++ LinkChat.Core/Implementations/ProtocolService.cs
@@ -6,6 +6,19 @@ namespace LinkChat.Core.Implementations;
 
 public class ProtocolService : IProtocolService
 {
+    public event Action<HeartbeatMessage>? HeartbeatFrameReceived;
+    public event Action<ChatAck>? ChatAckFrameReceived;
+    public event Action<FileAck>? FileAckFrameReceived;
+    public event Action<TextMessage>? TextMessageFrameReceived;
+    public event Action<FileStart>? FileStartFrameReceived;
+    public event Action<FileChunk>? FileChunkFrameReceived;
+    public event Action<MessageReaction>? MessageReactionFrameReceived;
+    public event Action<UserStatus>? UserStatusFrameReceived;
+
+    public ProtocolService(INetworkService networkService)
+    {
+        networkService.FrameReceived += OnFrameReceived;
+    }
     public byte[] CreateFrame(Message message)
     {
         var options = new JsonSerializerOptions
@@ -28,5 +41,48 @@ public class ProtocolService : IProtocolService
         string RecoveryMessage = Encoding.UTF8.GetString(frame);
         Message Message = JsonSerializer.Deserialize<Message>(RecoveryMessage, options);
         return Message;
+    }
+
+    private void OnFrameReceived(byte[] frameData)
+    {
+        Message? message = ParseFrame(frameData);
+
+        switch (message)
+        {
+            case ChatAck chatAck:
+                ChatAckFrameReceived?.Invoke(chatAck);
+                break;
+
+            case FileAck fileAck:
+                FileAckFrameReceived?.Invoke(fileAck);
+                break;
+
+            case FileChunk fileChunk:
+                FileChunkFrameReceived?.Invoke(fileChunk);
+                break;
+
+            case FileStart fileStart:
+                FileStartFrameReceived?.Invoke(fileStart);
+                break;
+
+            case HeartbeatMessage heartbeatMessage:
+                HeartbeatFrameReceived?.Invoke(heartbeatMessage);
+                break;
+
+            case MessageReaction messageReaction:
+                MessageReactionFrameReceived?.Invoke(messageReaction);
+                break;
+
+            case TextMessage textMessage:
+                TextMessageFrameReceived?.Invoke(textMessage);
+                break;
+
+            case UserStatus userStatus:
+                UserStatusFrameReceived?.Invoke(userStatus);
+                break;
+
+            default:
+                throw new Exception("The type of parsed message cannot be identified.");
+        }
     }
 }
