9f0e11ea0312f4485192b39c825a62362c6188e3
Rlianny <revelianny10@gmail.com>
2025-10-01 22:52:31 -0400

    Compilations Files Added

--- LinkChat.Infrastructure.Linux/NetworkServiceImplementation/NetworkService.cs
+++ LinkChat.Infrastructure.Linux/NetworkServiceImplementation/NetworkService.cs
@@ -1,9 +1,25 @@
+using LinkChat.Core.Models;
 using LinkChat.Core.Services;
 
 namespace LinkChat.Infrastructure
 {
-    public sealed class LinuxNetworkService
+    public sealed class LinuxNetworkService : INetworkService, IDisposable
     {
-
+        public event Action<byte[]>? HeartbeatFrameReceived;
+        public event Action<byte[]>? ChatAckFrameReceived;
+        public event Action<byte[]>? FileAckFrameReceived;
+        public event Action<byte[]>? TextMessageFrameReceived;
+        public event Action<byte[]>? FileStartFrameReceived;
+        public event Action<byte[]>? FileChunkFrameReceived;
+        public event Action<byte[]>? MessageReactionFrameReceived;
+        public event Action<byte[]>? UserStatusFrameReceived;
+        public void SendMessage(User user, Message message)
+        {
+            throw new NotImplementedException();
+        }
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
