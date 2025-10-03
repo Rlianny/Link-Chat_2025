4079884a9f24978e4a30e6eab58921f47759129f
KevinTorres01 <kevintorresperera@gmail.com>
2025-10-03 12:15:14 -0400

    Console App Update

--- LinkChat.Core/obj/Debug/net9.0/LinkChat.Core.AssemblyInfo.cs
+++ LinkChat.Core/obj/Debug/net9.0/LinkChat.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LinkChat.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bc5095e2c70f8fc97ff7c88b29973fbc070bced")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b05ab53573c31e4117b75fdc21d25511fc0b525e")]
 [assembly: System.Reflection.AssemblyProductAttribute("LinkChat.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LinkChat.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
